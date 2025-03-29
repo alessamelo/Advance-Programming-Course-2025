3d589c028024409c972f011bc997e79eb60ef3e4
alessamelo <alessamelo8@gmail.com>
2025-03-29 11:21:24 -0500

    Update Task2.1.cpp

--- Code_Challenge_L09/Task2.1.cpp
+++ Code_Challenge_L09/Task2.1.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <string>
+#include <concepts>
 
+//concepts
+template <typename T> 
+concept Numeric = std::integral<T> || std::floating_point<T>;
 
+template <Numeric T>
+T add(T a, T b){
+    return a+b;
+}
 
-int main(){
-    
+int main() {
+    int c1, c2;
+    double c3, c4;
 
+    std::cout << "Agregue dos numeros enteros: ";
+    std::cin >> c1 >> c2;
+
+    std::cout << "Agregue dos numeros flotantes: ";
+    std::cin >> c3 >> c4;
+
+    std::cout << "Suma de enteros: " << add(c1, c2) << "\n";
+    std::cout << "Suma de flotantes: " << add(c3, c4) << "\n";
+    return 0;
 }
