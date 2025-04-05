7da8092e97d2d0e61c622476b1d0a09a572c3534
alessamelo <alessamelo8@gmail.com>
2025-04-04 22:53:31 -0500

    Update user.h

--- Assignment05/user.h
+++ Assignment05/user.h
@@ -1,28 +1,43 @@
-/*
- * CS106L Assignment 5: TreeBook
- * Created by Fabio Ibanez with modifications by Jacob Roberts-Baca.
- */
+#ifndef USER_H
+#define USER_H
 
 #include <iostream>
 #include <string>
 
-class User
-{
+class User {
 public:
-  User(const std::string& name);
-  void add_friend(const std::string& name);
-  std::string get_name() const;
-  size_t size() const;
-  void set_friend(size_t index, const std::string& name);
-
-  /** 
-   * STUDENT TODO:
-   * Your custom operators and special member functions will go here!
-   */
+    explicit User(const std::string& name);
+    ~User();
+    
+    // Copy operations
+    User(const User& other);
+    User& operator=(const User& other);
+    
+    // Deleted move operations
+    User(User&& other) = delete;
+    User& operator=(User&& other) = delete;
+    
+    // Member functions
+    void add_friend(const std::string& name);
+    std::string get_name() const;
+    size_t size() const;
+    void set_friend(size_t index, const std::string& name);
+    
+    // Operators
+    User& operator+=(User& other);
+    bool operator<(const User& other) const;  // Added const
+    
+    // Friend declaration
+    friend std::ostream& operator<<(std::ostream& os, const User& user);
 
 private:
-  std::string _name;
-  std::string* _friends;
-  size_t _size;
-  size_t _capacity;
+    std::string _name;
+    std::string* _friends;
+    size_t _size;
+    size_t _capacity;
 };
+
+// Non-member function declaration
+std::ostream& operator<<(std::ostream& os, const User& user);
+
+#endif // USER_H
